efa160226e73a5e6c3fc92bd8bc6e464a71aac3d
KiruthikaRajasekaran <krithikasadhana213@gmail.com>
2016-07-09 11:51:58 +0530

    finish

---
+++ README.c
@@ -0,0 +1,19 @@
+# vowel
+#include<stdio.h>
+#include<conio.h>
+void min()
+{
+int a;
+printf("enter the letter");
+clrscr();
+if(a,e,i,o,u)
+{
+printf("the lettr is a vowel");
+}
+else
+{
+printf("the letter is not a vowel");
+}
+getch();
+}
+
